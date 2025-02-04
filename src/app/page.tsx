5991da7ccf54ba08c84c766ee7e4b26926eba97e
Ole August Støle <oleastole@gmail.com>
2025-02-04 15:24:50 +0100

    feat: rag example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,39 @@ import { useChat } from "ai/react";
 const apiRoutes = {
   basic: "/api/chat-v1",
   withTools: "/api/chat-v2",
+  rag: "/api/chat-v3",
 } as const;
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: apiRoutes.basic, // TODO: Toggle between the basic implementation and the one with tools
+    api: apiRoutes.rag, // Toggle between the basic implementation and the one with tools
     maxSteps: 10,
   });
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === "user" ? "User: " : "AI: "}
-          {m.toolInvocations ? (
-            <pre>{JSON.stringify(m.toolInvocations, null, 2)}</pre>
-          ) : (
-            <p>{m.content}</p>
-          )}
-        </div>
-      ))}
+      <div className="space-y-4">
+        {messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            <div>
+              <div className="font-bold">{m.role}</div>
+              <p>
+                {m.content.length > 0 ? (
+                  m.content
+                ) : (
+                  <span className="italic font-light">
+                    {"calling tool: " + m?.toolInvocations?.[0].toolName}
+                  </span>
+                )}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
 
       <form onSubmit={handleSubmit}>
         <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
