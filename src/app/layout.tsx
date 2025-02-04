e53dcd5e83b62ebbec70c760c2b601dfc3c6084e
oscarvik <oscar@askeladden.co>
2025-02-04 18:32:04 +0100

    feat: create landing page with AI tool template overview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50`}
       >
         {children}
       </body>
